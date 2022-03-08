bcf1cc3053031991b0d8bc8f5ad58fbcdf59d123
KevinPiriou <kevinpiriou@yahoo.fr>
2022-03-08 16:18:15 +0100

    add div score and manip dom

--- menu.js
+++ menu.js
@@ -1,10 +1,14 @@
+//// INIT CONST
 const gameNewButton = document.getElementById("btnNew"); // START BUTTON GAME
 const gameTest = document.getElementById("gameroom"); //TODO: CANVAS OR DOM ?? = GAME DISPLAY
-const gameExitButton = document.getElementById("btnExit"); // EXIT BUTTON GAME 
+const gameCreditButton = document.getElementById("btnCredit"); // EXIT BUTTON GAME 
 const gameDebug = document.getElementById("debug"); // LOG DEBUG ON GAME DISPLAY
 const menuTitle = document.getElementsByTagName('h1')[0] // TITLE GAME IN MENU
 const scoreGame = document.getElementById("scoreBoard"); // SCORE
+const credits = document.getElementById("credit");
+const paraCredit = document.createElement("p");
 
+//// FUNCTION
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -12,50 +16,54 @@ function sleep(milliseconds) {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
 }
-
-
-
-
-
+//// DOM
+/////// DOM START GAME
 gameNewButton.addEventListener('click', function(event) {
     event.preventDefault;
     // Hide Element 
     gameTest.style.backgroundImage = "none";
     gameNewButton.style.visibility = "hidden";
-    gameExitButton.style.visibility = "hidden";
+    gameCreditButton.style.visibility = "hidden";
     menuTitle.style.visibility = "hidden";
+    paraCredit.style.visibility = "hidden";
+    scoreGame.style.visibility = "hidden";
     // LOOP START GAME
     sleep(10);
     gameTest.style.borderRadius = "0%";
     gameTest.style.backgroundColor = "green"; //Test STARTGAME
     gameDebug.innerText = "Debug: RUNNING GAME <% //TODO: delete this %>";
 });
-
-gameExitButton.addEventListener('click', function(event) {
+/////// DOM CREDIT
+gameCreditButton.addEventListener('click', function(event) {
     event.preventDefault;
     // Hide Element
-    gameExitButton.style.visibility = "hidden";
+    gameCreditButton.style.visibility = "hidden";
     gameNewButton.style.visibility = "hidden";
     menuTitle.style.visibility = "hidden";
+    // Visible
+    paraCredit.style.visibility = "visible";
     // GAME BREAK
     sleep(10);
-    gameTest.style.backgroundColor = "red"; //Test EXIT GAME
-    gameDebug.innerText = "Debug: EXIT GAME <% //TODO: delete this %>"
+    gameTest.style.backgroundColor = "rgb(16, 24, 32)"; //CREDIT
+    gameDebug.innerText = "Debug: CREDIT  <% //TODO: delete this %>";
+    paraCredit.style.fontFamily = "Roboto";
+    paraCredit.innerText = "Merci d'avoir joué à ce magnifique jeu créer par Jordan et Kevin";
+    credits.appendChild(paraCredit);
 
 });
-
-
+/////// DOM LOSE GAME
 gameTest.addEventListener('dblclick', function(event) {
     event.preventDefault;
     // Replace Element
     gameTest.style.borderRadius = "90%";
     gameTest.style.background = "rgb(16, 24, 32)";
     gameTest.style.backgroundColor = "linear-gradient(0deg, rgba(16,24,32,1) 0%, rgba(33,37,41,1) 100%);"; //Test LOSE CONDITION GAME
-    gameExitButton.style.visibility = "visible";
+    gameCreditButton.style.visibility = "visible";
     gameNewButton.style.visibility = "visible";
     menuTitle.style.visibility = "visible";
+    paraCredit.style.visibility = "hidden";
+    scoreGame.style.visibility = "visible";
     // OUT LOOP CONDITION = PLAYER LOSING
     sleep(10);
     gameDebug.innerText = "Debug: LOSING GAME <% //TODO: delete this %>"
-
 });
